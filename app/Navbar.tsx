bca50743c6b02159e9085b40e8efc2205593b0a3
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2023-12-08 17:48:21 +0500

    creating home page layout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,15 +1,13 @@
 'use client'
-import Link from 'next/link'
-import React from 'react'
-import Logo from './components/Logo'
-import SocialMediaIcons from './components/SocialMediaIcons'
-
+import Link from 'next/link';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import Logo from './components/Logo';
 const Navbar = () => {
     return (
         <header className='w-full px-32 py-8 flex items-center justify-between'>
             <nav>
-                {pageLinks.map(link =>
-                    <Link className='mx-4 hover:text-amber-400' key={link.href} href={link.href}>
+                {pageLinks.map((link, index) =>
+                    <Link className='mx-4 hover:text-amber-400' key={index} href={link.href}>
                         {link.label}
                     </Link>
                 )}
@@ -17,8 +15,11 @@ const Navbar = () => {
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <Logo />
             </div>
-            <nav>
-                <SocialMediaIcons facebook={''} instagram={''} linkedin={''} twitter={''} />
+            <nav className='flex'>
+                <Link href='' className='mr-20' ><FaFacebook size={20}/></Link>
+                <Link href='' className='mr-20'><FaInstagram size={20}/></Link>
+                <Link href='' className='mr-20'><FaTwitter size={20}/></Link>
+                <Link href='' ><FaLinkedin size={20}/></Link>
             </nav>
         </header>
     )
@@ -30,10 +31,4 @@ const pageLinks = [
     { label: 'Articles', href: '/' },
 ]
 
-const socialLinks = [
-    { label: 'FB', href: '/' },
-    { label: 'Insta', href: '/' },
-    { label: 'Twitter', href: '/' },
-    { label: 'LinkedIn', href: '/' },
-]
 export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import developerImage from '../public/developer_image-removebg-preview.png'
 export default function Home() {
   return (
-    <div>Personal Portfolio</div>
+    <div>
+      <div className="flex gap-5 ml-20 pt-0">
+        <div className="flex items-center justify-between w-full">
+          <div className="w-1/2">
+            <Image src={developerImage} alt="developer" className="w-full h-auto pt-0" />
+          </div>
+          <div className="w-1/2">
+            <h1>React Application Development</h1>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
