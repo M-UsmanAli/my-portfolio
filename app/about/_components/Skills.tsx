3fda397644ae92dd70be0b7d808a67a397b38d5f
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-01-09 17:25:40 +0500

    adding motion to the skills

--- app/about/_components/Skills.tsx
+++ app/about/_components/Skills.tsx
@@ -1,13 +1,15 @@
+'use client'
 import React from 'react'
-
+import {motion} from 'framer-motion';
 const Skills = () => {
   return (
     <>
     <h2 className='font-bold text-8xl mt-44 w-full text-center'>Skills</h2>
     <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
-      <div className='flex items-center justify-center rounded-full font-semibold bg-black text-white'>
+      <motion.div className='flex items-center justify-center rounded-full font-semibold bg-black
+       text-white p-8 shadow-black' whileHover={{scale:1.05}}>
         Web
-      </div>
+      </motion.div>
 
     </div>
     </>
