d9991856908df123f9cd41cc6868b711e363a0bd
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-01-09 17:05:15 +0500

    margin was out

--- app/components/AnimatedHomeText.tsx
+++ app/components/AnimatedHomeText.tsx
@@ -7,7 +7,7 @@ interface Props {
 const AnimatedHomeText = ({ text }: { text: String }) => {
   return (
     <div className='w-full mx-auto flex items-center justify-center text-center overflow-hidden my-2'>
-      <h1 className='inline-block w-full text-black font-bold capitalize text-6xl !text-left transition-all duration-1000 ease-in-out delay-100'>
+      <h1 className='inline-block w-full text-black font-bold capitalize text-6xl !text-left transition-all duration-1000 ease-in-out delay-100 mb-2'>
         {text}
       </h1>
     </div>
