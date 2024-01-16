71396ae94eda451d988c8bb1b85ed7b5322395be
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-01-16 17:24:00 +0500

    created the featured image component

--- app/projects/_components/FeaturedProjects.tsx
+++ app/projects/_components/FeaturedProjects.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 interface Props {
     type: string,
@@ -14,10 +15,24 @@ const FeaturedProjects = ({ title, type, summary, img, link, github }: Props) =>
     return (
         <article>
             <Link href={link} target='_blank'>
-                <Image src={img} alt={title} className='w-full h-auto'/>
+                <Image src={img} alt={title} className='w-full h-auto' />
             </Link>
 
-            <div className=''></div>
+            <div className=''>
+                <span>{type}</span>
+                <Link href={link} target='_blank'>
+                    <h2>{title}</h2>
+                </Link>
+                <p>{summary}</p>
+                <div>
+                <Link href={github} target='_blank'>
+                    <FaGithub/>
+                </Link>
+                <Link href={github} target='_blank'>
+                    Visit the Project
+                </Link>
+                </div>
+            </div>
         </article>
     )
 }
