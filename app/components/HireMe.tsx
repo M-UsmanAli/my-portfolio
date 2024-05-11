be64a5934df29d89095d31664c2e071c6cb7e08a
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-11 14:43:47 +0500

    upadting by adding new features

--- app/components/HireMe.tsx
+++ app/components/HireMe.tsx
@@ -7,7 +7,7 @@ const HireMe = () => {
         <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
             <div className='w-48 h-auto flex items-center justify-center relative'>
                 <Image src={imageHireMe} alt='HireMe' className='fill-black animate-spin-slow' />
-                <Link href='https://www.fiverr.com/muhamusmanali?up_rollout=true' className='flex items-center justify-center absolute 
+                <Link href='https://www.upwork.com/freelancers/~017b90eda5610cdaca?viewMode=1' className='flex items-center justify-center absolute 
                 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                  bg-black text-white shadow-md border border-solid border-black
                 w-20 h-20 rounded-full font-semibold  hover:bg-white hover:text-black'>
