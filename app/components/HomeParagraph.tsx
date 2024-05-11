7aecef144e87731ea92e0b598efbafb37f0fac79
M-UsmanAli <aliusmanmuhammad98@gmail.com>
2024-05-11 16:11:55 +0500

    getting done the deplyong chnages

--- app/components/HomeParagraph.tsx
+++ app/components/HomeParagraph.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const HomeParagraph = () => {
     return (
         <div>
             <p className="mr-9 text-justify">
                 Welcome to my world of React development, where I merge creativity with technology to
-                build immersive web experiences. I'm Muhammad Usman Ali, a dedicated developer driven by a
+                build immersive web experiences. I&apos;m Muhammad Usman Ali, a dedicated developer driven by a
                 passion for innovation and a commitment to excellence. With a strong foundation in
                 React, I bring ideas to life, creating applications that leave a lasting impact.
                 Explore my portfolio and witness the art of digital transformation.
--- app/components/NextIcon.tsx
+++ app/components/NextIcon.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import nextIcon from '../../public/bulb.svg'
 import Image from 'next/image'
+import nextIcon from '../../public/bulb.svg'
 const NextIcon = () => {
     return (
         <div className='absolute right-8 bottom-1 inline-block w-24'>
